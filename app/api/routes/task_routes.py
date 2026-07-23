702af3b4894cd5eee72efbbb1158227c16c14753
Jhune Wally Esparcia <jhuneesparcia1@gmail.com>
2026-07-24 05:40:09 +0800

    Stage 2: read endpoints with 404

--- app/api/routes/task_routes.py
+++ app/api/routes/task_routes.py
@@ -1,14 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter(
     prefix = "/tasks",
     tags = ["Tasks"]
 )
 
+tasks = [
+        {"id": 1, "title": "Go to gym.", "done": True},
+        {"id": 2, "title": "Go for a walk.", "done": True},
+        {"id": 3, "title": "Go to sleep.", "done": False}
+    ]
+
 @router.get("/")
 def get_tasks():
-    return {
-        "name": "Task API",
-        "version": "1.0",
-        "endpoints": ["/tasks"]
-    }
+    return tasks
+
+@router.get("/{task_id}")
+def get_task_by_id(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    
+    raise HTTPException(
+        status_code = 404,
+        detail = f"Task {task_id} not found."
+    )
